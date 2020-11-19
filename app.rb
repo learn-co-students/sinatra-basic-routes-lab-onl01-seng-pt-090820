363f50aaa36a886fef4dc0685acac2659991a956
samara-amanda <samara.amanda01@gmail.com>
2020-11-19 17:20:28 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Samara"
+    end
+
+    get '/hometown' do
+        "My hometown is New York"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is He Wasn't Man Enough"
+    end
+
 end
