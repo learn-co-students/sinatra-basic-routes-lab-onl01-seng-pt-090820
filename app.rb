735286c4007d21a06a01618a3233dbe243f0db3f
Mikelanne <mikelanne.wood@gmail.com>
2020-11-29 16:15:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello World"
+      end
+
+    get '/name' do
+        "My name is Mikelanne."
+    end 
+
+    get '/hometown' do
+        "My hometown is Boston."
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Little Things."
+    end 
+
 end
