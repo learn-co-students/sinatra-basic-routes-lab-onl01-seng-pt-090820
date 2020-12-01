f4537087d83a80439af471a3a11cd4559ed1f8f7
conner fortner <fortner.conner@yahoo.com>
2020-12-01 23:00:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    #sets up homepage
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+        "My name is Conner Fortner"
+    end 
+
+    get '/hometown' do
+        "My hometown is Salisbury, North Carolina"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Still Feel, by Half Alive"
+    end 
+
 end
