46e65f32f52b647c0cd91951749caa08ef12a175
Christian Simon <csimon1127@gmail.com>
2020-12-09 20:52:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Rome, GA!"
+  end
+  
+  get '/name' do 
+    "My name is Christian!"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is non-existent, but I love the 80's."
+  end
+  
+  
 end
